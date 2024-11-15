a2731b3450fe7d9793b971fb7f6d29b98710dff1
ParkieV <st-psk@mail.ru>
2024-11-15 13:39:46 +0300

    Add README

--- create_local_user.js
+++ create_local_user.js
@@ -2,11 +2,11 @@ const localUser = process.env.DB_USERNAME;
 const localPassword = process.env.DB_PASSWORD;
 const localDb = process.env.DB_NAME;
 
-db = db.getSiblingDB(localDb); // Переключение на указанную базу данных
+db = db.getSiblingDB(localDb);
 db.createUser({
   user: localUser,
   pwd: localPassword,
   roles: [
-    { role: "readWrite", db: localDb } // Назначаем права readWrite для базы данных
+    { role: "readWrite", db: localDb }
   ]
 });
