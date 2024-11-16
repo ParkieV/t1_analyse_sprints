4c2318d24bbc851d9890ae4ce0b98d43673dc7a1
Tim Danilov <volinadtim@gmail.com>
2024-11-16 15:11:32 +0300

    Sprint estimation

--- frontend/src/pages/sprint-detail/sprint-detail-main/sprint-estimation/sprint-estimation.component.ts
+++ frontend/src/pages/sprint-detail/sprint-detail-main/sprint-estimation/sprint-estimation.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SprintEstimationChartComponent } from "./sprint-estimation-chart/sprint-estimation-chart.component";
 
 @Component({
   selector: 'app-sprint-estimation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SprintEstimationChartComponent],
   templateUrl: './sprint-estimation.component.html',
-  styleUrl: './sprint-estimation.component.scss'
+  styleUrl: './sprint-estimation.component.scss',
 })
 export class SprintEstimationComponent {
+  estimationDate: Date = new Date();
 
+  comment: string = 'на 3 дня раньше запланированного';
 }
