d4f94a68ee770abbe78dcfb746df1607fc47fd7b
ParkieV <st-psk@mail.ru>
2024-11-16 09:46:33 +0300

    Fix backend and ml

--- ml/API/main.py
+++ ml/API/main.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 from routes.base import base_router
 load_dotenv()
 
-app = FastAPI(title="T1 Impulse Hack ml http server", docs_url="/docs", openapi_url="/api")
+app = FastAPI(title="T1 Impulse Hack ml http server",root_path="/api_ml")
 
-app.include_router(base_router, prefix="/api/base", tags=["base"])
+app.include_router(base_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=os.getenv("HOST"), port=8000, reload=True)
+    uvicorn.run("main:app", host=os.getenv("HOST"), port=8010)
